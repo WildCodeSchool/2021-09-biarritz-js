c6a4da288d391aeb396f7480bec067581c115813
Lydie Pluvinage <lydiepluvinage@yahoo.fr>
2021-11-21 15:25:19 +0100

    New workshop : exercices on GoT quotes

---
+++ workshops/react-exercices-got/src/data.js
@@ -0,0 +1,8 @@
+const data = [
+    "A lion does not concern himself with the opinion of sheep.",
+    "Any man who must say 'I am the King' is no true King.",
+    "Every day that he remains a prisoner, the less our name commands respect.",
+    "I need you to become the man you were always meant to be. Not next year, not tomorrow, now."
+    ];
+
+    export default data;
